f400b56ded6fadf516e04e6e7bdee9da5e6740bb
José Andrés Durand <joseandresdurand@gmail.com>
2025-09-16 22:46:49 -0500

    avance de ejercicios

--- cycle-02/Programming-I/02-loops/practice-02/ejercicio-26.cpp
+++ cycle-02/Programming-I/02-loops/practice-02/ejercicio-26.cpp
@@ -9,11 +9,12 @@
  * @problem 26. Escriba un programa que despliegue todas las permutaciones de los 
  *          números del 1 a n. Por ejemplo, si n = 3, debe obtenerse:
  *          1 2 3
- *          1 3 2
- *          2 1 3
- *          2 3 1
- *          3 1 2
  *          3 2 1
+ *          3 1 2
+ *          2 1 3
+ *          2 3 1      
+ *          1 3 2
+ *          
  *******************************************************************************
  * -----------------------------------------------------------------------------
  * ANALYSIS & DESIGN
@@ -56,9 +57,21 @@
     }
 
     for (int i=1; i<=numFactorial;i++){
+        cout<<i<<": ";
         for (int j=1; j<=num;j++){
-            cout<<j<<" ";
+            int valor = (i+j) % num + 1;
+            cout<<valor<<" "; 
+            
+            /*
+            1: 3 1 2
+            2: 1 2 3
+            3: 2 3 1
+            Lograr invertir cada una y se tiene todas las combinaciones.            
+            */
+            
+            
         }
+        
         cout<<endl;
     }
 
