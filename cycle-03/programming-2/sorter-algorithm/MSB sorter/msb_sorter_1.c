4f21402fc7dc9231f467a9604ec99833f2857bc5
JoseAndresDurand <joseandresdurand@gmail.com>
2026-06-03 22:54:31 -0500

    2da implementación de msb_sorter_1

--- cycle-03/programming-2/sorter-algorithm/MSB sorter/msb_sorter_1.c
+++ cycle-03/programming-2/sorter-algorithm/MSB sorter/msb_sorter_1.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
-void numero_a_bits(int m, int n, int arr[], int matrix[][n]);
 void imprimir(int arr[], int n);
-void imprimir_matrix(int m, int n, int arr[][n]);
-void msb_sort_1(int m, int n, int arr[]);
+void msb_sort(int m, int n, int arr[]);
 
 int main() {
   int arr[] = {9, 7, 1, 13, 11};
@@ -11,47 +9,34 @@ int main() {
   int n = 4; // numero de bit
   int matrix[m][n];
 
-  numero_a_bits(m, n, arr, matrix);
-
   printf("%s\n", "Array original:");
   imprimir(arr, m);
   printf("\n");
-  printf("%s\n", "Matrix original:");
-  imprimir_matrix(m, n, matrix);
-  printf("\n");
 
-  msb_sort_1(m, n, arr);
+  msb_sort(m, n, arr);
   printf("%s\n", "Array con msb_sort:");
-  imprimir(arr, n);
+  imprimir(arr, m);
 
   return 0;
 }
 
-void msb_sort_1(int m, int n, int arr[]) {
+void msb_sort(int m, int n, int arr[]) {
   int mascara = 8;
   int min = 0;
   int max = m - 1;
-  while (min > max) {
-    if (((arr[min] & mascara) == 8) ^ ((arr[max] & mascara) == 8)) {
+  while (min < max) {
+    while (min < max && ((arr[min] & mascara) == 0)) {
+      min++;
+    }
+    while (min < max && ((arr[max] & mascara) == mascara)) {
+      max--;
+    }
+    if (min < max) {
       int temp = arr[min];
       arr[min] = arr[max];
       arr[max] = temp;
-      min++;
       max--;
-    }
-    if (((arr[min] & mascara) == 8) && ((arr[max] & mascara) == 8)) {
       min++;
-    }
-  }
-}
-
-void numero_a_bits(int m, int n, int arr[], int matrix[][n]) {
-  int mascara = 1;
-  for (int i = 0; i < m; i++) {
-    int numero = arr[i];
-    for (int j = n; j > 0; j--) {
-      matrix[i][j - 1] = numero & mascara;
-      numero = numero >> 1;
     }
   }
 }
@@ -61,13 +46,4 @@ void imprimir(int arr[], int n) {
     printf("%d ", arr[i]);
   }
   printf("\n");
-}
-
-void imprimir_matrix(int m, int n, int arr[][n]) {
-  for (int i = 0; i < m; i++) {
-    for (int j = 0; j < n; j++) {
-      printf("%d ", arr[i][j]);
-    }
-    printf("\n");
-  }
 }
