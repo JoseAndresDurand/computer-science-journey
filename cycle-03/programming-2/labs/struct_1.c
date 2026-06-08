5dbcfd4ef663434fa880fa9ef11daf137dc0712d
JoseAndresDurand <joseandresdurand@gmail.com>
2026-06-08 13:47:14 -0500

    lab 08-06

---
+++ cycle-03/programming-2/labs/struct_1.c
@@ -0,0 +1,71 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAXWORD 100
+
+struct key {
+  char *word;
+  int count;
+} keytab[] = {"auto", 0, "break", 0, "casa", 0, "ciudad", 0};
+
+#define NKEYS (sizeof(keytab) / sizeof(keytab[0]))
+
+int getword(char *, int);
+int binsearch(char *, struct key[], int);
+
+int main() {
+  int n;
+  char word[MAXWORD];
+
+  while (getword(word, MAXWORD) != EOF)
+    if (isalpha(word[0]))
+      if ((n = binsearch(word, keytab, NKEYS)) >= 0)
+        keytab[n].count++;
+
+  for (n = 0; n < NKEYS; n++)
+    if (keytab[n].count > 0)
+      printf("%4d %s\n", keytab[n].count, keytab[n].word);
+  return 0;
+}
+
+int binsearch(char *word, struct key tab[], int n) {
+  int cond;
+  int low, high, mid;
+  low = 0;
+  high = n - 1;
+  while (low <= high) {
+    mid = (low + high) / 2;
+    if ((cond = strcmp(word, tab[mid].word)) < 0)
+      high = mid - 1;
+    else if (cond > 0)
+      low = mid + 1;
+    else
+      return mid;
+  }
+  return -1;
+}
+
+int getword(char *word, int lim) {
+  int c;
+  char *w = word;
+  while (isspace(c = getchar())) {
+    ;
+  }
+  if (c != EOF) {
+    *w++ = c;
+  }
+  if (!isalpha(c)) {
+    *w = '\0';
+    return c;
+  }
+  for (; --lim > 0; w++) {
+    if (!isalnum(*w = getchar())) {
+      ungetc(*w, stdin);
+      break;
+    }
+  }
+
+  *w = '\0';
+  return word[0];
+}
